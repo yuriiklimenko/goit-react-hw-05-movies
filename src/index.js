ed6b7311a0d3f06da65112b2bdfcfc2b1a6735d0
yuriiklimenko <109259174+yuriiklimenko@users.noreply.github.com>
2023-02-03 23:37:46 +0200

    init

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/goit-react-hw-05-movies">
-      <App />
-    </BrowserRouter>
+    {/* <BrowserRouter basename="/goit-react-hw-05-movies"> */}
+    <App />
+    {/* </BrowserRouter> */}
   </React.StrictMode>
 );
